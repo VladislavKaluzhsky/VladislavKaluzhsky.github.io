6d614fcfaeb7401d809a75d0f1e3d07dbeaa7f7f
vlad <vlad@vlad>
2017-04-05 16:12:16 +0300

    commit3

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-		$(".humb").click(function(){
-		$(".nav").toggleClass("active");
-	});
-		$(".humb").click(function(){
-		$(".humb").toggleClass("rotate");
-	});
-});
-
